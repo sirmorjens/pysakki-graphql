beaa8225199ea93a27660364c798d0ef13abd393
Joni Haiko <Joni.Haiko@student.lab.fi>
2026-05-25 09:12:55 +0300

    pysähdysaikojen if lauseiden sievennys

--- src/Stoptime.tsx
+++ src/Stoptime.tsx
@@ -27,11 +27,11 @@ export default function Stoptime({stoptime}: Props)
         const realSeconds = data.realtimeArrival!.valueOf();
         const scheduledSeconds = data.scheduledArrival!.valueOf();
 
-        let realTime = "";
-        let scheduledTime = new Date(scheduledSeconds * 1000).toISOString().slice(11, 16); // hh:mm
+        let shownTime = new Date(scheduledSeconds * 1000).toISOString().slice(11, 16); // hh:mm
 
         let cancelState= "";
         let arrivalState= "";
+        let aikaClass = "aika"
 
         let d = new Date();
         let secondsNow = Math.round(d.getTime() / 1000)%86400;
@@ -39,31 +39,25 @@ export default function Stoptime({stoptime}: Props)
         // merkitään bussi saapuvaksi jos saapumisaika lähestyy
         if((realSeconds-3600*3)-secondsNow <= 30) { arrivalState = "Saapuu" }
 
-        if(data.realtimeState == "CANCELED") { cancelState = "Peruttu" }
-
         if(realSeconds != scheduledSeconds) {
+            aikaClass = "oikeaaika"
             // pyöristää ylös lähimpään minuuttiin
-            if(realSeconds%60>30) { realTime = new Date((realSeconds + 60-realSeconds%60) * 1000).toISOString().slice(11, 16); }
+            if(realSeconds%60>30) { shownTime = new Date((realSeconds + 60-realSeconds%60) * 1000).toISOString().slice(11, 16); }
 
-            else { realTime = new Date(realSeconds * 1000).toISOString().slice(11, 16); }
+            else { shownTime = new Date(realSeconds * 1000).toISOString().slice(11, 16); }
 
-            return( // palauttaa päivitetyn ajan jos sellainen löytyy
-                    // reittikoodi, määränpää, aika (reaali), onko vuoro peruttu
-                <p className="trip">
-                    <b className="reittikoodi">{data.trip!.routeShortName}</b> <br />
-                    <b className="paikka">{data.headsign}</b> <br />
-                    <b className="oikeaaika">{realTime}</b>
-                    <b className="reittihäiriö"><span>{cancelState}</span></b>
-                    <b className="saapumistilanne">{arrivalState}</b>
-                </p> 
-            )
-        }else return( // reittikoodi, määränpää, aika (suunniteltu), onko vuoro peruttu
+            if(data.realtimeState == "CANCELED") { cancelState = "Peruttu"; aikaClass = "peruttuaika" }
+
+        } 
+
+        return( // reittikoodi, määränpää, aika (suunniteltu), onko vuoro peruttu
             <p className="trip">
                 <b className="reittikoodi">{data.trip!.routeShortName}</b> <br />
                 <b className="paikka">{data.headsign}</b> <br />
-                <b className="aika">{scheduledTime}</b>
-                <b className="reittihäiriö"><span>{cancelState}</span></b>
+                <b className={aikaClass}>{shownTime}</b>
+                <b className="reittihäiriö">{cancelState}</b>
                 <b className="saapumistilanne">{arrivalState}</b>
-            </p>) 
+            </p>
+        ) 
 
     };
