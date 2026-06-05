aeead409bcb7a9fb2c79b51277b8ce01403980aa
Ville <ville@villekoivuranta.fi>
2026-06-05 15:01:41 +0300

    Footer completed, responsive sizes contd.

--- src/LR_components/LR_Footer.tsx
+++ src/LR_components/LR_Footer.tsx
@@ -1,11 +1,16 @@
 import LRstyle from './Styles/LRstyle.module.css'
+import palaute from '../assets/palaute.svg'
+import temp_logo from '../assets/temp_logo.png'
 
 export default function LR_Footer () {
 
     return (
         <div className={LRstyle.footer}>
-            <div> Teksti + QR</div>
-            <div>Logo</div>
+            <div className={LRstyle.cta}>
+                <div><img src={palaute} alt="Puhekupla" /></div>
+                <div><p>Kerro mielipiteesi pysäkkinäytöstä<br /><b>lsl.fi/asiakaspalvelu/palaute</b></p></div>
+            </div>
+            <div className={LRstyle.logo}><img src={temp_logo} alt="LSL" /></div>
         </div>
 
     )
