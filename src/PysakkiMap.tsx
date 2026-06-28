23e9749ba28a3c81a0f45a0a3c189b32614c0bf2
Ville <ville@villekoivuranta.fi>
2026-06-29 02:02:28 +0300

    Exported some functionality to clear up the mess

--- src/PysakkiMap.tsx
+++ src/PysakkiMap.tsx
@@ -17,7 +17,7 @@ import mapstyle from "./Map/pysakki_mapstyle.json"
 import { useFragment } from 'react-relay';
 import { graphql, useLazyLoadQuery } from 'react-relay';
 
-import { clampedToViewArea } from './PysakkiMapUtils'
+import { clampedToViewArea, getNextNominalColor } from './PysakkiMapUtils'
 
 import { PysakkiSettings } from './PysakkiSettings';
 
@@ -184,7 +184,7 @@ export default function PysakkiMap() {
       refresh()
     }, refreshRateSec)
 
-    return () => clearTimeout(timerId)
+    return () => clearInterval(timerId)
   }, [])
 
 
@@ -465,61 +465,9 @@ const updateMap = () => {
 
     return FeatureObj;
   } 
-  // @ts-expect-error // unused for bw screen
-  const xx_routeNominalColorPalette: string[] = [
-    '#fff7ec',
-    '#fee8c8',
-    '#fdd49e',
-    '#fdbb84',
-    '#fc8d59',
-    '#ef6548',
-    '#d7301f',
-    '#b30000',
-    '#7f0000']
-  
-  // @ts-expect-error unused color palette for visual route separation
-  const x_routeNominalColorPalette: string[] = [
-    "rgba(141, 211, 199, 0.4)",
-    "rgba(255, 255, 179, 0.4)",
-    "rgba(190, 186, 218, 0.4)",
-    "rgba(251, 128, 114, 0.4)",
-    "rgba(128, 177, 211, 0.4)",
-    "rgba(253, 180, 98, 0.4)",
-    "rgba(179, 222, 105, 0.4)",
-    "rgba(252, 205, 229, 0.4)",
-    "rgba(217, 217, 217, 0.4)",
-    "rgba(188, 128, 189, 0.4)",
-    "rgba(204, 235, 197, 0.4)",
-    "rgba(255, 237, 111, 0.4)"
-  ]
 
-  const routeNominalStyle: {color: string, width: number}[] = [
-    {
-      color: "rgb(0, 0, 0)",
-      width: 1,
-    },
-    {
-      color: "rgb(139, 139, 139)",
-      width: 3,
-    }
-  ]
 
   const mapBoundsOffset = 0.055
-
-  let nominalStyleIndex = 0;
-  const getNextNominalColor = (): {
-    color: string,
-    index: number,
-    width: number,
-  } => {
-    nominalStyleIndex = nominalStyleIndex+1 >= routeNominalStyle.length ? 0 : nominalStyleIndex + 1;
-
-    return {
-      color: routeNominalStyle[nominalStyleIndex].color,
-      width: routeNominalStyle[nominalStyleIndex].width,
-      index: nominalStyleIndex,
-    };
-  }
 
   return (
     <div className="mapContainer">
--- src/PysakkiMapUtils.ts
+++ src/PysakkiMapUtils.ts
@@ -11,3 +11,59 @@ export const clampedToViewArea = (coords: Position): {
 
     return [{value: coords[0]}, {value: coords[1]}]
 } 
+
+
+let nominalStyleIndex = 0;
+
+// @ts-expect-error // unused for bw screen
+const xx_routeNominalColorPalette: string[] = [
+'#fff7ec',
+'#fee8c8',
+'#fdd49e',
+'#fdbb84',
+'#fc8d59',
+'#ef6548',
+'#d7301f',
+'#b30000',
+'#7f0000']
+
+// @ts-expect-error unused color palette for visual route separation
+const x_routeNominalColorPalette: string[] = [
+"rgba(141, 211, 199, 0.4)",
+"rgba(255, 255, 179, 0.4)",
+"rgba(190, 186, 218, 0.4)",
+"rgba(251, 128, 114, 0.4)",
+"rgba(128, 177, 211, 0.4)",
+"rgba(253, 180, 98, 0.4)",
+"rgba(179, 222, 105, 0.4)",
+"rgba(252, 205, 229, 0.4)",
+"rgba(217, 217, 217, 0.4)",
+"rgba(188, 128, 189, 0.4)",
+"rgba(204, 235, 197, 0.4)",
+"rgba(255, 237, 111, 0.4)"
+]
+
+const routeNominalStyle: {color: string, width: number}[] = [
+{
+    color: "rgb(0, 0, 0)",
+    width: 1,
+},
+{
+    color: "rgb(139, 139, 139)",
+    width: 3,
+}
+]
+
+export const getNextNominalColor = (): {
+    color: string,
+    index: number,
+    width: number,
+} => {
+    nominalStyleIndex = nominalStyleIndex+1 >= routeNominalStyle.length ? 0 : nominalStyleIndex + 1;
+
+    return {
+        color: routeNominalStyle[nominalStyleIndex].color,
+        width: routeNominalStyle[nominalStyleIndex].width,
+        index: nominalStyleIndex,
+    };
+}
