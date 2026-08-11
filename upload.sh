abe5cda691b1a4f1dc83d233538d8978f60a2ef7
Ville <ville.koivuranta@gmail.com>
2026-08-11 05:28:35 +0300

    Simple deployment script requires .env file with upload path now

--- upload.sh
+++ upload.sh
@@ -1,5 +1,13 @@
+#!/bin/sh
 # Simple deployment
 
+if ! uploadPath=$(cat "./.env.uploadPath") 2>/dev/null; then
+    echo "Save ssh path into '.env.uploadPath'"
+    echo "Example: sshUserName@serverdomain.com:path/to/upload/folder"
+    exit
+fi
+
 uploadPath=$(cat ./.env.uploadPath)
+
 npm run build && scp -r dist/* $uploadPath
 
